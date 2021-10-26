2708b3e644c3682590c7587c845ba0808f335153
Mateus Gonçalves Franco <mattfran67@gmail.com>
2021-10-26 18:21:31 -0300

    modified background color

--- src/components/App.js
+++ src/components/App.js
@@ -6,27 +6,51 @@ import {
   Redirect
 } from "react-router-dom"
 
-import { Home } from "components/Home"
 import { Quiz } from "components/Quiz"
+import { Home } from "components/Home"
 import { Result } from "components/Result"
+import { Container, styled, Typography } from "@mui/material"
+
+const WallPaper = styled('div')({
+  position: 'fixed',
+  width: '100%',
+  height: '100%',
+  top: 0,
+  left: 0,
+  overflow: 'hidden',
+  background: '#474141',
+  zIndex: -1,
+})
 
 const App = () => {
   return (
     <Router>
-      <Switch>
-        <Route exact path="/">
-          <Home />
-        </Route>
-        <Route exact path="/quiz">
-          <Quiz />
-        </Route>
-        <Route exact path="/result">
-          <Result />
-        </Route>
-        <Route path="*">
-          <Redirect to="/" />
-        </Route>
-      </Switch>
+      <WallPaper />
+      <Typography 
+        variant="h2"
+        textAlign="center"
+        component="h1"
+        color="white"
+        p={3}
+      >
+        Quiz App
+      </Typography>
+      <Container disableGutters sx={{ mb: 2 }}>
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route exact path="/quiz">
+            <Quiz />
+          </Route>
+          <Route exact path="/result">
+            <Result />
+          </Route>
+          <Route path="*">
+            <Redirect to="/" />
+          </Route>
+        </Switch>
+      </Container>
     </Router>
   )
 }
