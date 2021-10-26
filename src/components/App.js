2340f3208423ad17a80fd2af613124214b12ffbc
Mateus Gonçalves Franco <mattfran67@gmail.com>
2021-10-26 17:43:45 -0300

    created main routes

---
+++ src/components/App.js
@@ -0,0 +1,34 @@
+import React from "react"
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect
+} from "react-router-dom"
+
+// import { Home } from "Home"
+// import { Quiz } from "Quiz"
+// import { Result } from "Result"
+
+const App = () => {
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          {/* <Home /> */}
+        </Route>
+        <Route exact path="/quiz">
+          {/* <Quiz /> */}
+        </Route>
+        <Route exact path="/result">
+          {/* <Result /> */}
+        </Route>
+        <Route path="*">
+          <Redirect to="/" />
+        </Route>
+      </Switch>
+    </Router>
+  )
+}
+
+export default App
